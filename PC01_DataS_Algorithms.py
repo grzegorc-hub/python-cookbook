6bd24208f133101c22077955b3c98d71ddac51ac
Maciej <grzegorc1@gmail.com>
2024-09-27 01:48:51 +0200

    .

--- PC01_DataS_Algorithms.py
+++ PC01_DataS_Algorithms.py
@@ -145,3 +145,51 @@ for key in d:
 
 r = json.dumps(d)
 print(r)
+
+
+# 1.8 CALC WITH DICTONARIES
+# --------------------------
+# In order to perform useful calculations on the dictionary contents, it is often useful to
+# invert the keys and values of the dictionary using zip().
+
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+min_price = min(zip(prices.values(), prices.keys()))
+print(min_price)
+
+prices_sorted = sorted(zip(prices.values(), prices.keys()))
+print(prices_sorted)
+
+# zip creates iterator that can be only consumed once
+prices_and_names = zip(prices.values(), prices.keys())
+print(min(prices_and_names))  # OK
+#print(max(prices_and_names))  # ValueError: max() arg is an empty sequence
+
+
+# 1.9 FINDING COMMONALITIES IN 2 DICTs
+# -------------------------------------
+
+a = {
+    'x': 1,
+    'y': 2,
+    'z': 3
+}
+
+b = {
+    'w': 10,
+    'x': 11,
+    'y': 2
+}
+
+print( a.keys() & b.keys() )
+print( a.keys() - b.keys() )
+print( a.items() & b.items() )
+
+c = { k: a[k] for k in a.keys() - {'z'} }
+print(c)
